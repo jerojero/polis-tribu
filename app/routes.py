c30857242dffdd6aff2229ef702e41af081a4298
Jero <jeronimo.barrios.s@gmail.com>
2021-09-14 18:54:21 +1000

    v0.2 Implemented a basic xid polis, javascript not yet working

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}")
+        return redirect(f"/polis/xid/{form.username.data}")
+    return render_template('index.html', form=form)
+
+
+@app.route('/polis/xid/<username>')
+def polis(username):
+    return render_template('polis.html', username=username)
