066fb225385d581590067522cc11338b0fb3bf66
Jero <jeronimo.barrios.s@gmail.com>
2021-09-25 21:42:51 +1000

    v0.97.6 Fixed config again

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -12,9 +14,9 @@ class Config(object):
 
     # Internationalization
     LANGUGAGES = ['en', 'es']
-    
+
     # Email Configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
