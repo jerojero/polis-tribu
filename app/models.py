bd177514b12e271d749b89482f9928682516a2c9
Jero <jeronimo.barrios.s@gmail.com>
2021-09-25 19:40:22 +1000

    v0.97.4 Fixed email support

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 # Flask stuff
 from app import db
 from app import login
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -30,12 +31,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
+            current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except Exception:
             return
