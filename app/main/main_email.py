32bbdfbb9bba91f9b73e25ca44fbea78ff8784e5
Jeronimo <jeronimo.barrios.s@gmail.com>
2021-10-27 11:12:12 +1100

    v0.9.7 - Changed email settings

--- app/main/main_email.py
+++ app/main/main_email.py
@@ -4,7 +4,7 @@ from app.email import send_email
 
 def send_automated_email(email_to_send, email_title, email_address, time, **kwargs):
     send_email(email_title,
-               sender=current_app.config['ADMINS'][0],
+               sender=current_app.config['MAIL_USERNAME'],
                recipients=[email_address],
                text_body=render_template(
                    f'email/{email_to_send}.txt', **kwargs),
